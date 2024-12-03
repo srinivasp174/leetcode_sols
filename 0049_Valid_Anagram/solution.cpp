29076c41cb9119000e14557b1935a4c854051230
Srinivas Charan Peethala <nt5srinivaspeethala@gmail.com>
2024-12-03 19:52:18 +0530

    Runtime: 0ms (100.00%) | Memory: 9.57MB (44.67%)

---
+++ 0049_Valid_Anagram/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> count(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
