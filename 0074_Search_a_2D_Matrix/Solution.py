28a89b83af252b9923ad06300f678e54b3f26276
Srinivas Charan Peethala <nt5srinivaspeethala@gmail.com>
2025-01-09 22:01:55 +0530

    Runtime: 0ms (100.00%) | Memory: 18.24MB (16.59%)

---
+++ 0074_Search_a_2D_Matrix/Solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        top, bot = 0, rows - 1
+        while top <= bot:
+            row = (top + bot)  // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        if not (top <= bot):
+            return False
+        row = (top + bot) // 2
+        l, r = 0, cols - 1
+        while l <= r:
+            col = (l + r) // 2
+            if target == matrix[row][col]:
+                return True
+            elif target < matrix[row][col]:
+                r = col - 1
+            else:
+                l = col + 1
+        return False
